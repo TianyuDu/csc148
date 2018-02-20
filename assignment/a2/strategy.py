d618b094c8053fe2a297b276890d031282438c96
MasterDoo <masterdu9951@icloud.com>
2018-02-20 03:15:51 -0500

    Adding Existing files

--- assignment/a2/strategy.py
+++ assignment/a2/strategy.py
@@ -24,31 +24,105 @@ def recursive_minimax(game: Game):
     """
     candidate = game.current_state.get_possible_moves()
     canditate_prop = [
-        rec_find_max(game, game.current_state.make_move(move))
+        -1 * get_state_score(game, game.current_state.make_move(move))
+        #  get_state_score() provides the STATE SCORE for the OPPO player
+        #  in each resultent state.
+        #  -1 * ... to calculate the STATE SCORE for player taking action now.
         for move in candidate
     ]
     max_index = canditate_prop.index(max(canditate_prop))
     return candidate[max_index]
 
 
-def rec_find_max(game: Game, state: GameState):
+def get_state_score(game, state):
+    """
+    Helper function
+    """
     if game.is_over(state):  # Base case, leaf case.
         if game.is_winner(state.get_current_player_name()):
-            return 1  # if current player is the winner, get point of 1.
+            return 1  # Current player wins, the STATE SCORE for
+        # current player is 1.
         else:
-            return -1  # if current player is the loser, get point of -1.
+            return -1  # Oppo player wins, the STATE SCORE for
+        # current player is -1
     else:
         return max([
-            -1 * rec_find_max(game, state.make_move(move))
+            -1 * get_state_score(game, state.make_move(move))
+            # -1 * ... reflects the the MOVE SCORE for certain move
+            # for current player
             for move in state.get_possible_moves()
-        ])
+        ])  # the max of MOVE SCORE becomes the STATE SCORE for current state
+        #  for current player.
 
 
 def iterative_minimax(game: Game) -> Any:
     """
     Doc String Omitted.
     """
-    pass
+    state = game.current_state
+    candidate_moves = state.get_possible_moves()
+    current_player = state.get_current_player_name()
+    trees = [
+        TreeNode(state.make_move(move))
+        for move in candidate_moves
+    ]  # Create tree nodes for candidate moves.
+    for root in trees:
+        # stack = [
+        #     TreeNode(root.state.make_move(move))
+        #     for move in root.state.get_possible_moves()
+        # ]
+        #
+        # if game.is_over(root.state):
+        #     game.current_state = root.state
+        #     if game.is_winner(current_player):
+        #         root.score = 1
+        #     else:
+        #         root.score = -1
+        #     continue
+        # for sub in stack:
+        #     root.add_child(sub)
+        stack = [root]  # Create current stack.
+        while stack != []:
+            exam_node = stack.pop()  # Take out the last node.
+            if game.is_over(exam_node.state):
+                game.current_state = exam_node.state
+                if game.is_winner(current_player):
+                    exam_node.score = 1
+                else:
+                    exam_node.score = -1
+            else:
+                if exam_node.children == []:
+                    possible_moves = exam_node.state.get_possible_moves()
+                    for m in possible_moves:
+                        c_node = TreeNode(exam_node.state.make_move(m))
+                        exam_node.add_child(c_node)
+                    stack.append(exam_node)
+                    for child in exam_node.children:
+                        stack.append(child)
+                else:
+                    exam_node.score = exam_node.get_high_score()
+    scores = [root.score for root in trees]
+    max_idx = scores.index(max(scores))
+    return candidate_moves[max_idx]
+
+
+class TreeNode():
+    def __init__(self, state: GameState):
+        self.state = state
+        self.children = []
+        self.score = -99
+
+    def add_child(self, c: "TreeNode"):
+        self.children.append(c)
+
+    def get_high_score(self):
+        try:
+            return max([
+                child.score
+                for child in self.children
+            ])
+        except ValueError:
+            pass
 
 if __name__ == "__main__":
     from python_ta import check_all
