4501b8d8b75ba15ae4f50a7ac4668fa731a07eea
EC2 Default User <ec2-user@ip-172-31-24-149.ec2.internal>
2018-03-01 01:26:11 +0000

    Adding Existing files

---
+++ feb282018_0501.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+class Tree:
+    pass
+
+
+def levelorder_visit(t: Tree, act: Callable[[Tree], Any]) -> None:
+    """
+    Visit every node in Tree t in level order and act on the node
+    as you visit it.
+    >>> t = descendants_from_list(Tree(0),
+                                  [1, 2, 3, 4, 5, 6, 7], 3)
+    >>> def act(node): print(node.value)
+    >>> levelorder_visit(t, act)
+    """
+    to_act = Queue()
+    to_act.add(t)
+    while not to_act.is_empty():
+        tree = to_act.remove()
+        act(tree)
+        for child in tree.children:
+            to_act.add(child)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod
